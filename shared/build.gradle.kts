3c3ea1a510b8b9348cbebb42f4796723e3606fca
Felix Leyva <felix.leyva@adesso.de>
2026-04-04 19:55:04 +0200

    main: Add wasmJs target to shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
@@ -29,6 +30,10 @@ kotlin {
     jvm()
 
     js {
+        browser()
+    }
+
+    @OptIn(ExperimentalWasmDsl::class) wasmJs {
         browser()
     }
 
